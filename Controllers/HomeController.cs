b2f3092b18a7dfbc7f3fe43da2911d02a9c3a2c6
moreiraaline <aline.moreira.1526789@sga.pucminas.br>
2024-10-20 17:07:10 -0300

    Adicionar arquivo .gitignore e remover diretórios indesejados do controle de versão

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using guiaUnivesitario.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace guiaUnivesitario.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Homepage"); // Modifique aqui
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace guiaUnivesitario.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View(); // Certifique-se de que exista uma View chamada Login.cshtml
+        }
+    }
+}
